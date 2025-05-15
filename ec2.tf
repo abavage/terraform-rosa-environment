3e1df21deeaa9095b4c885b9099d1a080c39250b
Andrew Bavage <abavage@gmail.com>
2025-05-15 10:46:55 +1000

    updates

--- ec2.tf
+++ ec2.tf
@@ -3,18 +3,33 @@ resource "aws_key_pair" "ssh_key" {
   public_key = file("~/.ssh/one_id_rsa.pub")
 }
 
+# Changed to use parameter store
 data "aws_ami" "amazon-linux" {
   most_recent = true
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.6.2024*-x86_64"]
+    values = ["al2023-ami-2023.7.2025*-x86_64"]
   }
 }
 
+output "ami_id" {
+  value = data.aws_ami.amazon-linux.id
+}
+
+
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+output "param_id" {
+  value = nonsensitive(data.aws_ssm_parameter.al2023.value)
+  #sensitive = true
+}
 
 resource "aws_instance" "vm" {
-  ami           = data.aws_ami.amazon-linux.id
+  #ami           = data.aws_ami.amazon-linux.id
+  ami           = data.aws_ssm_parameter.al2023.value
   subnet_id      = tolist(data.aws_subnets.rosa_public_subnets.ids)[0]
   vpc_security_group_ids = [aws_security_group.ssh.id]
   instance_type = "t3.micro"
