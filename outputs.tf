d39dcffa0141197edf05860568dd375557665718
Andrew Bavage <abavage@gmail.com>
2025-08-11 08:16:59 +1000

    clean up

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,7 @@
-#data "aws_vpc" "get-rosa-vpc" {
-#  filter {
-#    name = "tag:Name"
-#    values = ["rosa-private-three"]
-#  }
-#}
-#
-#output "got-the-rosa-vpc" {
-#  value = data.aws_vpc.get-rosa-vpc.id
-#}
-
-
-
 output "ab_rosa_public_subnet" {
   #value       = aws_subnet.rosa_public_subnets[*] # works
   value       = [for s in aws_subnet.rosa_public_subnets : s.id]
   description = "AB     ---- The ID of the subnet."
-}
-
-
-#output "rosa_public_subnet_subnet_ids" {
-#  value = [for s in data.ab_rosa_public_subnet : s.id]
-#  description  = "THE ID MAYBE"
-#}
-
-output "ab_rosa_private_subnet" {
-  #value       =     aws_subnet.rosa_private_subnet[*].id
-  value       = aws_subnet.rosa_private_subnets[*]
-  description = "AB     ---- The ID of the private subnet."
 }
 
 
@@ -53,64 +28,3 @@ output "aws_nat_gateways" {
 output "subnet_length" {
   value = length(var.rosa_public_subnet)
 }
-
-
-
-
-# get the subnets
-#data "aws_subnets" "rosa_public_subnet" {
-#  filter {
-#    #name   = "vpc-id"
-#    #values = [aws_vpc.vpc.id]
-#    name   = "tag:Name"
-#    values = ["rosa-public-subnet-*"]
-#  }
-#depends_on = [aws_subnet.rosa_public_subnets]
-#}
-
-#data "aws_subnet" "rosa_public_subnet" {
-#  for_each = toset(data.aws_subnets.rosa_public_subnet.ids)
-#  id       = each.value
-#}
-
-#output "rosa_public_subnet_subnet_ids" {
-#  value = [for s in data.aws_subnet.rosa_public_subnet : s.id]
-#}
-
-#data "aws_subnets" "rosa_private_subnet" {
-#  filter {
-#    #name   = "vpc-id"
-#    #values = [aws_vpc.vpc.id]
-#    name   = "tag:Name"
-#    values = ["rosa-private-subnet-*"]
-#  }
-#depends_on = [aws_subnet.rosa_private_subnets]
-#}
-
-#data "aws_subnet" "rosa_private_subnet" {
-#  for_each = toset(data.aws_subnets.rosa_private_subnet.ids)
-#  id       = each.value
-#}
-
-#output "rosa_private_subnet_subnet_ids" {
-#  value = [for s in data.aws_subnet.rosa_private_subnet : s.id]
-#}
-
-
-#########################
-
-#output "ids" {
-#  value = [aws_nat_gateway.nat.*.id]
-#}
-
-
-#data "aws_eip" "nat_eips" {
-#  filter {
-#    name   = "tag:Name"
-#    values = ["nat-*"]
-#  }
-#}
-
-#output "aws_eip_nat_ips" {
-#  value = [for i in data.aws_eip.nat_eips : i.id]
-#}
